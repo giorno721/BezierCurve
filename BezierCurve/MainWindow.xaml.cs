00b6aefc2e758f690d9394622c7ef47e53f4509c
Roman <roman.marushko.pz.2022@lpnu.ua>
2024-03-11 17:18:12 +0200

    v1.0

--- BezierCurve/MainWindow.xaml.cs
+++ BezierCurve/MainWindow.xaml.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -24,6 +24,8 @@ namespace BezierCurve
         public ObservableCollection<Point> Points { get; set; }
         bool Inited = false;
         private bool HyperLines = false;
+        private double Scale = 10;
+        private Point Center;
         public MainWindow()
         {
             InitializeComponent();
@@ -36,7 +38,9 @@ namespace BezierCurve
         }
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            AddCoordinateGrid();
+            Center = new Point(canvas.ActualWidth / 2, canvas.ActualHeight / 2);
+            AddRandomPoints();
+            FillCanvans();
         }
         private void AddRandomPoints()
         {
@@ -51,21 +55,40 @@ namespace BezierCurve
         private void FillCanvans()
         {
             canvas.Children.Clear();
-            AddCoordinateGrid();
+            DrawAxes();
             DrawPoints();
             DrawLines();
             DrawCurve();
 
         }
+
         private void DrawCurve()
         {
             if (Points.Count < 3)
             {
                 return;
             }
-            int numPoints = 50;
-            Point[] points = BezierCurveWindowHelper.CalculatePoints(HyperLines ? 1000 : numPoints, Points);
-            DrawCurveLine(points);
+            int numPoints = 100;
+            Point[] virtualPoints = BezierCurveWindowHelper.CalculatePoints(HyperLines ? 1000 : numPoints, Points);
+            Point[] screenPoints = virtualPoints.Select(VirtualCoordsToScreenCords).ToArray();
+            DrawCurveLine(screenPoints);
+            DrawCurvePoints(screenPoints);
+        }
+
+        private void DrawCurvePoints(Point[] points)
+        {
+            for (int i = 0; i < points.Length; i++)
+            {
+                Ellipse ellipse = new Ellipse
+                {
+                    Width = 3,
+                    Height = 3,
+                    Fill = Brushes.Black
+                };
+                canvas.Children.Add(ellipse);
+                Canvas.SetLeft(ellipse, points[i].X - ellipse.Width / 2);
+                Canvas.SetTop(ellipse, points[i].Y - ellipse.Height / 2);
+            }
         }
 
         private void DrawCurveLine(Point[] points)
@@ -84,26 +107,31 @@ namespace BezierCurve
                 canvas.Children.Add(line);
             }
         }
+
         private void DrawLines()
         {
             for (int i = 1; i < Points.Count; i++)
             {
+                Point screenPoint1 = VirtualCoordsToScreenCords(Points[i - 1]);
+                Point screenPoint2 = VirtualCoordsToScreenCords(Points[i]);
                 Line line = new Line
                 {
-                    X1 = Points[i - 1].X,
-                    Y1 = Points[i - 1].Y,
-                    X2 = Points[i].X,
-                    Y2 = Points[i].Y,
+                    X1 = screenPoint1.X,
+                    Y1 = screenPoint1.Y,
+                    X2 = screenPoint2.X,
+                    Y2 = screenPoint2.Y,
                     Stroke = Brushes.Blue,
                     StrokeThickness = 1
                 };
                 canvas.Children.Add(line);
             }
         }
+
         private void DrawPoints()
         {
-            foreach (var point in Points)
+            foreach (var virtualPoint in Points)
             {
+                Point screenPoint = VirtualCoordsToScreenCords(virtualPoint);
                 Ellipse ellipse = new Ellipse
                 {
                     Width = 10,
@@ -111,8 +139,8 @@ namespace BezierCurve
                     Fill = Brushes.Black
                 };
                 canvas.Children.Add(ellipse);
-                Canvas.SetLeft(ellipse, point.X - ellipse.Width / 2);
-                Canvas.SetTop(ellipse, point.Y - ellipse.Height / 2);
+                Canvas.SetLeft(ellipse, screenPoint.X - ellipse.Width / 2);
+                Canvas.SetTop(ellipse, screenPoint.Y - ellipse.Height / 2);
             }
         }
         private void GenerateRandomBezierCurve(object sender, RoutedEventArgs e)
@@ -120,41 +148,157 @@ namespace BezierCurve
             Points.Clear();
             canvas.Children.Clear();
             AddRandomPoints();
+
         }
-        private void AddCoordinateGrid()
+        private void DrawAxes()
         {
-            // Vertical lines (X-axis)
-            for (double x = 100; x <= canvas.ActualWidth; x += 50) // Змінено крок на 50
+            DrawXYLines();
+            DrawTicks();
+            DrawNames();
+            DrawArrows();
+        }
+
+        private void DrawArrows()
+        {
+            // Стрілочка на осі X
+            Polygon arrowX = new()
             {
-                Line line = new Line
+                Points = new PointCollection(new Point[]
                 {
-                    X1 = x,
-                    Y1 = canvas.ActualHeight - 100, // Змінено на висоту площини мінус 100
-                    X2 = x,
-                    Y2 = canvas.ActualHeight - 90, // Змінено на висоту площини мінус 90 (змініть, якщо потрібно)
-                    Stroke = Brushes.LightGray,
-                    StrokeThickness = 1
+            new Point(canvas.ActualWidth, Center.Y),
+            new Point(canvas.ActualWidth - 10, Center.Y - 5),
+            new Point(canvas.ActualWidth - 10, Center.Y + 5)
+            }
+            ),
+                Fill = Brushes.Black
+            };
+            canvas.Children.Add(arrowX);
+
+            // Стрілочка на осі Y
+            Polygon arrowY = new Polygon
+            {
+                Points = new PointCollection(new Point[] {
+            new Point(Center.X, 0),
+            new Point(Center.X - 5, 10),
+            new Point(Center.X + 5, 10)
+            }),
+                Fill = Brushes.Black
+            };
+            canvas.Children.Add(arrowY);
+        }
+
+        private void DrawNames()
+        {
+            Point point = new(canvas.ActualWidth - 20, Center.Y + 10);
+            Label label = new()
+            {
+                Content = "X",
+                Foreground = Brushes.Black
+            };
+            canvas.Children.Add(label);
+            Canvas.SetLeft(label, point.X);
+            Canvas.SetTop(label, point.Y);
+
+            point = new Point(Center.X - 20, 5);
+            label = new()
+            {
+                Content = "Y",
+                Foreground = Brushes.Black
+            };
+            canvas.Children.Add(label);
+            Canvas.SetLeft(label, point.X);
+            Canvas.SetTop(label, point.Y);
+        }
+
+        private void DrawTicks()
+        {
+            // Позначки на осі X
+            for (double x = Scale; x < Center.X; x += Scale)
+            {   
+                if (2 * x < canvas.ActualWidth - 20)
+                {
+                    Line tickRight = new()
+                    {
+                        X1 = Center.X + x,
+                        Y1 = Center.Y - 5,
+                        X2 = Center.X + x,
+                        Y2 = Center.Y + 5,
+                        Stroke = Brushes.Black
+                    };
+                    canvas.Children.Add(tickRight);
+                }
+
+                Line tickLeft = new()
+                {
+                    X1 = Center.X - x,
+                    Y1 = Center.Y - 5,
+                    X2 = Center.X - x,
+                    Y2 = Center.Y + 5,
+                    Stroke = Brushes.Black
                 };
-                canvas.Children.Add(line);
+                canvas.Children.Add(tickLeft);
             }
 
 
-            // Horizontal lines (Y-axis)
-            for (double y = canvas.ActualHeight - 100; y >= 0; y -= 50) // Змінено крок на 50
+
+            // Позначки на осі Y
+            for (double y = Scale; y < Center.Y; y += Scale)
             {
-                Line line = new Line
+                Line tickDown = new()
                 {
-                    X1 = 100, // Змінено на ширину площини
-                    Y1 = y,
-                    X2 = 110, // Змінено на ширину площини плюс 10 (змініть, якщо потрібно)
-                    Y2 = y,
-                    Stroke = Brushes.LightGray,
-                    StrokeThickness = 1
-                };
-                canvas.Children.Add(line);
+                    X1 = Center.X - 5,
+                    Y1 = Center.Y + y,
+                    X2 = Center.X + 5,
+                    Y2 = Center.Y + y,
+                    Stroke = Brushes.Black
+                };  
+                canvas.Children.Add(tickDown);
+
+                if (2 * y < canvas.ActualHeight - 20)
+                {
+                    Line tickUp = new()
+                    {
+                        X1 = Center.X - 5,
+                        Y1 = Center.Y - y,
+                        X2 = Center.X + 5,
+                        Y2 = Center.Y - y,
+                        Stroke = Brushes.Black
+                    };
+                    canvas.Children.Add(tickUp);
+                }
             }
         }
 
+        private void DrawXYLines()
+        {
+            // Ось X
+            Line xAxis = new()
+            {
+                X1 = 0,
+                Y1 = Center.Y,
+                X2 = canvas.ActualWidth,
+                Y2 = Center.Y,
+                Stroke = Brushes.Black
+            };
+            canvas.Children.Add(xAxis);
+
+            // Ось Y
+            Line yAxis = new()
+            {
+                X1 = Center.X,
+                Y1 = 0,
+                X2 = Center.X,
+                Y2 = canvas.ActualHeight,
+                Stroke = Brushes.Black
+            };
+            canvas.Children.Add(yAxis);
+        }
+
+
+        private Point VirtualCoordsToScreenCords(Point vitualPoint)
+        {
+            return new Point(Center.X + vitualPoint.X * Scale, Center.Y + vitualPoint.Y * Scale);
+        }
 
     }
 }
